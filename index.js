974e3733b740e220c04620df9bba36ab50ec3a0a
thenabilarta <thenabilarta@gmail.com>
2020-09-04 14:54:18 +0700

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({nama: 'nabil'});
+  res.send({lala: 'lisa'});
 });
 
 const PORT = process.env.PORT || 5000;
