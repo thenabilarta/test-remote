80612a94cf3a714541dd838e40fef57ab68ede1b
thenabilarta <thenabilarta@gmail.com>
2020-09-07 11:15:15 +0700

    changed jennie

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({lala: 'lisa'});
+  res.send({jennie: 'ruby jane'});
 });
 
 const PORT = process.env.PORT || 5000;
