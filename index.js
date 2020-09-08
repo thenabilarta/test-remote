d89d7d1fda1c7d324cce933f3af9851ae4919259
thenabilarta <thenabilarta@gmail.com>
2020-09-08 15:55:39 +0700

    final dong

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // authRoutes(app);
 require('./routes/authRoutes')(app);
 
